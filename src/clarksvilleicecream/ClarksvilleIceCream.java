38692c3d00bf4a4600438dcf7064d452ab805e8e
PC <PC@192.168.194.28>
2014-03-12 16:21:39 -0500

    Guardando cambios en el repositorio

---
+++ src/clarksvilleicecream/ClarksvilleIceCream.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clarksvilleicecream;
+
+/**
+ *
+ * @author PC
+ */
+public class ClarksvilleIceCream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Crea un objeto de la clase IceCream y lo coloca visible.
+        IceCream ice = new IceCream();
+        ice.setVisible(true);
+    }
+    
+}
