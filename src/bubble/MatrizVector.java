7a9349cc669841394283b07609c1066136ce764a
PC <PC@192.168.193.207>
2014-03-19 17:48:27 -0500

    Convertir de vector a matriz

---
+++ src/bubble/MatrizVector.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bubble;
+
+/**
+ *
+ * @author Alvaro Niño
+ */
+public class MatrizVector {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Crea objeto de la clase Sort y lo coloca visible
+        Sort sort = new Sort();
+        sort.setVisible(true);
+    }
+    
+}
